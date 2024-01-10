66e62b2ffa4df1a1195a4e1565a06b179c3d5ef9
Benjamin Guma <benjaminguma.bg@gmail.com>
2024-01-10 15:55:36 +0100

    checked writers code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,101 @@
-import Image from 'next/image'
+"use client";
+import { useRef, useState } from "react";
+
+const downloadFile = async (URL: string, filename: string) => {
+	var dlAnchorElem = document.createElement("a");
+	document.body.appendChild(dlAnchorElem);
+	dlAnchorElem.setAttribute("href", URL);
+	dlAnchorElem.setAttribute("download", filename);
+	dlAnchorElem.click();
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [isRecording, setIsRecording] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const recordedData = useRef<Blob[]>([]);
+	const recorderRef = useRef<MediaRecorder | null>(null);
+	const screenRecordingStream = useRef<MediaStream | null>(null);
+	const videoRef = useRef<HTMLVideoElement | null>(null);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const startRecording = async () => {
+		const stream = await requestPermissionFromUserToAccessScreen();
+		recordedData.current = [];
+		recorderRef.current = new MediaRecorder(stream);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		recorderRef.current.addEventListener("dataavailable", collectVideoData);
+		recorderRef.current.addEventListener("stop", () => setIsRecording(false));
+		recorderRef.current.addEventListener("start", () => setIsRecording(true));
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		recorderRef.current.start();
+	};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	const requestPermissionFromUserToAccessScreen = async () => {
+		const stream: MediaStream = await navigator.mediaDevices.getDisplayMedia({
+			video: true,
+			audio: {
+				noiseSuppression: true,
+			},
+		});
+		screenRecordingStream.current = stream;
+		if (videoRef.current) {
+			videoRef.current.srcObject = stream;
+			videoRef.current.play();
+		}
+		return stream;
+	};
+
+	const collectVideoData = (ev: BlobEvent) => {
+		recordedData.current.push(ev.data);
+	};
+
+	const stopRecording = async () => {
+		const recorder = recorderRef.current;
+		if (recorder && videoRef.current) {
+			recorder.stop();
+			screenRecordingStream.current?.getTracks().map((track) => {
+				track.stop();
+				return;
+			});
+			videoRef.current.srcObject = null;
+		}
+	};
+
+	const downloadVideo = () => {
+		const videoBlob = new Blob(recordedData.current, {
+			type: recordedData.current[0].type,
+		});
+		const downloadURL = window.URL.createObjectURL(videoBlob);
+		downloadFile(downloadURL, `my-video.mkv`);
+
+		window.URL.revokeObjectURL(downloadURL);
+	};
+
+	return (
+		<main className='flex flex-col items-center gap-6 justify-center min-h-[100vh]'>
+			<div className='mt-8'>
+				{isRecording ? (
+					<>
+						<button onClick={stopRecording} className='btn btn-error'>
+							{" "}
+							stop recording
+						</button>
+					</>
+				) : (
+					<div className='flex gap-4 items-center'>
+						<button onClick={startRecording} className='btn btn-active btn-neutral'>
+							record screen
+						</button>
+
+						{recordedData.current.length ? (
+							<button onClick={downloadVideo} className='btn btn-active btn-neutral'>
+								download video
+							</button>
+						) : null}
+					</div>
+				)}
+			</div>
+
+			<video ref={videoRef} width={900} muted></video>
+		</main>
+	);
 }
